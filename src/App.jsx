832bc518382043c4115c099a99a96e93c81401d2
lcsilva76 <lsilva@fiap.com.br>
2023-07-28 16:19:26 -0300

    Mudança 1

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Só para ver se tá tudo ok!!!
         </p>
       </div>
       <p className="read-the-docs">
